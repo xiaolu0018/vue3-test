50d62edfddee116b3814ac9edc416d240f28534d
lxl <xiaolu00185@163.com>
2025-07-04 10:48:02 +0800

    eslint和prettier配置

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,11 +11,45 @@ module.exports = {
   plugins: ['vue', '@typescript-eslint'],
   parserOptions: {
     ecmaVersion: 2022,
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
   rules: {
     'vue/multi-word-component-names': 'off', // 允许单文件组件名[5](@ref)
-    'prettier/prettier': ['error', { singleQuote: true, htmlWhitespaceSensitivity: 'ignore' }], // 强制单引号[7](@ref)
-    'vue/html-closing-bracket-newline': ['error', { multiline: 'never' }]
-  }
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: false,
+        htmlWhitespaceSensitivity: 'ignore',
+        trailingComma: 'none',
+        endOfLine: 'auto'
+      }
+    ], // 强制单引号[7](@ref)
+    'vue/html-closing-bracket-newline': ['error', { multiline: 'never' }],
+    // 关闭与 Prettier 冲突的规则
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off'
+  },
+  // 新增：解决 Vue 文件中 TypeScript 解析问题
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      node: true
+    },
+    vue: {
+      version: '3.x'
+    }
+  },
+  ignorePatterns: [
+    '.eslintrc.cjs',
+    'node_modules/',
+    'dist/',
+    'public/',
+    'src/assets/*',
+    'script.js',
+    'dist.zip'
+  ]
 }
--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,12 +1,12 @@
-
 module.exports = {
-  "printWidth": 100,
-  "tabWidth": 2,
-  "useTabs": false,
-  "singleQuote": true,
-  "semi": false,
-  "trailingComma": "none",
-  "bracketSpacing": true,
-  "vueIndentScriptAndStyle": true, // 关闭 Vue 文件 script/style 标签缩进干扰[1,5](@ref)
-  "htmlWhitespaceSensitivity": "ignore" // 忽略 HTML 空格敏感性[1,5](@ref)
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  vueIndentScriptAndStyle: true, // 关闭 Vue 文件 script/style 标签缩进干扰[1,5](@ref)
+  htmlWhitespaceSensitivity: 'ignore', // 忽略 HTML 空格敏感性[1,5](@ref)
+  endOfLine: 'auto'
 }
